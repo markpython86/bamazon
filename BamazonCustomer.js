c0792d1d4b1b80c40ecefcee7b20afa6fdb32476
Mark <mark.faraj86@gmail.com>
2018-12-30 14:04:20 -0600

    finishing the customer app

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,17 +1,13 @@
 var mysql = require("mysql");
 var inquirer = require('inquirer');
-// var Table = require('cli-table');
 var displayTable = require('./TableDisplay.js');
 
 var connection = mysql.createConnection({
     host: "localhost",
-
     // Your port; if not 3306
     port: 3306,
-
     // Your username
     user: "root",
-
     // Your password
     password: "root",
     database: "bamazon"
@@ -25,11 +21,12 @@ connection.connect(function (err) {
 
 var displayProducts = function () {
     var display = new displayTable();
-    connection.query('SELECT * FROM products', function (err, response) {
-        if (err) throw err;
+    connection.query('SELECT * FROM products', function (error, response) {
+        if (error) throw error;
         console.log("connected as id " + connection.threadId + "\n");
         display.displayInventoryTable(response);
-        connection.end();
+        // connection.end();
+        itemPurchase();
     });
 }
 
@@ -52,6 +49,62 @@ var customerPrompt = function () {
         }
     })
 };
+
+var itemPurchase = function () {
+    inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "Enter the Item ID That you would like to puchase",
+        validate: function (value) {
+            var pass = value.match(
+                /[1-9]/g
+            );
+            if (pass) {
+                return true;
+            }
+            return 'Please enter a valid number';
+        }
+    }, {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to purchase?",
+        validate: function (value) {
+            var pass = value.match(
+                /[1-9]/g
+            );
+            if (pass) {
+                return true;
+            }
+
+            return 'Please enter a valid number';
+        }
+  
+    }]).then(function (answer) {
+        // Query the database for info about the item including the quantity currently in stock.
+        connection.query('SELECT product_name, department_name, price, stock_quantity FROM products WHERE ?', { item_id: answer.id }, function (error, response) {
+            console.log('\n  You would like to buy ' + answer.quantity + ' ' + response[0].product_name + ' ' + response[0].department_name + ' at $' + response[0].price + ' each');
+            // customerPrompt();
+            if (response[0].stock_quantity >= answer.quantity) {
+                let purchasedItemQuantity = response[0].stock_quantity - answer.quantity;
+                connection.query("UPDATE products SET ? WHERE ?", [
+                    {
+                        stock_quantity: purchasedItemQuantity
+                    }, {
+                        item_id: answer.id
+                    }], function (error, response) {
+                    });
+                let itemCost = response[0].price * answer.quantity;
+                console.log('\n  Order has been fulfilled! Your total cost is $' + itemCost.toFixed(2) + '\n');
+                // Order completed
+                customerPrompt();
+            } else {
+                console.log('\n  Sorry, Insufficient quantity in our stock.\n');
+                // Order not completed
+                customerPrompt();
+            }
+        });
+    });
+}
 
 
 
--- TableDisplay.js
+++ TableDisplay.js
@@ -4,15 +4,15 @@ Table = require('cli-table');
 var displayTable = function() {
 
     this.table = new Table({
-        head: ['Item ID', 'Product Name', 'Price', 'Stock Quantity'],
+        head: ['Item ID', 'Product Name', 'Category', 'Price', 'Stock Quantity'],
     });
 
     this.displayInventoryTable = function(response) {
     	this.response = response;
-        
+
 	    for (var index=0; index <this.response.length; index++) {
 	        this.table.push(
-	            [this.response[index].item_id, this.response[index].product_name, '$'+ this.response[index].price, this.response[index].stock_quantity] );
+	            [this.response[index].item_id, this.response[index].product_name, this.response[index].department_name, '$'+ this.response[index].price, this.response[index].stock_quantity] );
 	    }
     	console.log('\n' + this.table);
 	};
