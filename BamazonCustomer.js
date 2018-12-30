b25d3e9a1cc02589fda49a9452b47c8e3ad53067
Mark <mark.faraj86@gmail.com>
2018-12-29 22:04:10 -0600

    adding the customer prompt function

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -4,34 +4,56 @@ var inquirer = require('inquirer');
 var displayTable = require('./TableDisplay.js');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "root",
-  database: "bamazon"
+    // Your password
+    password: "root",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    displayProducts();
+    // Start app by Prompting the customer
+    customerPrompt();
 });
 
-var displayProducts = function() {
-	var display = new displayTable();
-	connection.query('SELECT * FROM products', function(err, response){
+var displayProducts = function () {
+    var display = new displayTable();
+    connection.query('SELECT * FROM products', function (err, response) {
         if (err) throw err;
         console.log("connected as id " + connection.threadId + "\n");
-		display.displayInventoryTable(response);
+        display.displayInventoryTable(response);
         connection.end();
-	});
+    });
 }
+
+var customerPrompt = function () {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "Would like to continue shopping?\n",
+        choices: ["Yes", "No"]
+    }).then(function (answer) {
+        switch (answer.action) {
+            case 'Yes':
+                displayProducts();
+                break;
+            case 'No':
+                connection.end();
+                break;
+            default:
+                break;
+        }
+    })
+};
+
+
 
 
 
